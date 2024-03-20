0e45aa3856f2a27cee4d8ff484d9660bf8361586
LiuQ <1639316332@qq.com>
2024-03-20 14:36:20 +0800

    feat:活动模块开发(中奖列表、活动信息、活动列表、奖品信息)

--- frontend/api/src/main/java/com/itheima/prize/api/action/GameController.java
+++ frontend/api/src/main/java/com/itheima/prize/api/action/GameController.java
@@ -1,5 +1,7 @@
 package com.itheima.prize.api.action;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.prize.commons.db.entity.CardGame;
@@ -40,46 +42,66 @@ public class GameController {
     @GetMapping("/list/{status}/{curpage}/{limit}")
     @ApiOperation(value = "活动列表")
     @ApiImplicitParams({
-            @ApiImplicitParam(name="status",value = "活动状态（-1=全部，0=未开始，1=进行中，2=已结束）",example = "-1",required = true),
-            @ApiImplicitParam(name = "curpage",value = "第几页",defaultValue = "1",dataType = "int", example = "1",required = true),
-            @ApiImplicitParam(name = "limit",value = "每页条数",defaultValue = "10",dataType = "int",example = "3",required = true)
+            @ApiImplicitParam(name = "status", value = "活动状态（-1=全部，0=未开始，1=进行中，2=已结束）", example = "-1", required = true),
+            @ApiImplicitParam(name = "curpage", value = "第几页", defaultValue = "1", dataType = "int", example = "1", required = true),
+            @ApiImplicitParam(name = "limit", value = "每页条数", defaultValue = "10", dataType = "int", example = "3", required = true)
     })
-    public ApiResult list(@PathVariable int status,@PathVariable int curpage,@PathVariable int limit) {
-        //TODO
-        return null;
+    public ApiResult list(@PathVariable int status, @PathVariable int curpage, @PathVariable int limit) {
+        LambdaQueryWrapper<CardGame> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        Date now = new Date();
+        System.out.println("now = " + now);
+        switch (status) {
+            // 全部
+            case -1:
+                lambdaQueryWrapper.orderByDesc(CardGame::getEndtime);
+                break;
+            // 未开始
+            case 0:
+                lambdaQueryWrapper.ge(CardGame::getStarttime, now).orderByAsc(CardGame::getStarttime);
+                break;
+            // 进行中
+            case 1:
+                lambdaQueryWrapper.le(CardGame::getStarttime, now).ge(CardGame::getEndtime, now);
+                break;
+            // 已结束
+            case 2:
+                lambdaQueryWrapper.le(CardGame::getEndtime, now).orderByDesc(CardGame::getEndtime);
+                break;
+        }
+        Page<CardGame> page = gameService.page(new Page<>(curpage, limit), lambdaQueryWrapper);
+        return new ApiResult(1, "成功", new PageBean<>(page));
     }
 
     @GetMapping("/info/{gameid}")
     @ApiOperation(value = "活动信息")
     @ApiImplicitParams({
-            @ApiImplicitParam(name="gameid",value = "活动id",example = "1",required = true)
+            @ApiImplicitParam(name = "gameid", value = "活动id", example = "1", required = true)
     })
     public ApiResult<CardGame> info(@PathVariable int gameid) {
-        //TODO
-        return null;
+        return new ApiResult<>(1, "成功", gameService.getById(gameid));
     }
 
     @GetMapping("/products/{gameid}")
     @ApiOperation(value = "奖品信息")
     @ApiImplicitParams({
-            @ApiImplicitParam(name="gameid",value = "活动id",example = "1",required = true)
+            @ApiImplicitParam(name = "gameid", value = "活动id", example = "1", required = true)
     })
     public ApiResult<List<CardProductDto>> products(@PathVariable int gameid) {
-        //TODO
-        return null;
+        return new ApiResult<>(1, "成功", loadService.getByGameId(gameid));
     }
 
     @GetMapping("/hit/{gameid}/{curpage}/{limit}")
     @ApiOperation(value = "中奖列表")
     @ApiImplicitParams({
-            @ApiImplicitParam(name="gameid",value = "活动id",dataType = "int",example = "1",required = true),
-            @ApiImplicitParam(name = "curpage",value = "第几页",defaultValue = "1",dataType = "int", example = "1",required = true),
-            @ApiImplicitParam(name = "limit",value = "每页条数",defaultValue = "10",dataType = "int",example = "3",required = true)
+            @ApiImplicitParam(name = "gameid", value = "活动id", dataType = "int", example = "1", required = true),
+            @ApiImplicitParam(name = "curpage", value = "第几页", defaultValue = "1", dataType = "int", example = "1", required = true),
+            @ApiImplicitParam(name = "limit", value = "每页条数", defaultValue = "10", dataType = "int", example = "3", required = true)
     })
-    public ApiResult<PageBean<ViewCardUserHit>> hit(@PathVariable int gameid,@PathVariable int curpage,@PathVariable int limit) {
-        //TODO
-        return null;
+    public ApiResult<PageBean<ViewCardUserHit>> hit(@PathVariable int gameid, @PathVariable int curpage, @PathVariable int limit) {
+        LambdaQueryWrapper<ViewCardUserHit> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(ViewCardUserHit::getGameid, gameid);
+        return new ApiResult<>(1,
+                "成功",
+                new PageBean<ViewCardUserHit>(hitService.page(new Page<>(curpage, limit), lambdaQueryWrapper)));
     }
-
-
 }
